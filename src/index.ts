3a181c8c586076c37a637d2242a666bdd8fcf37d
HDasilva59 <dasilva.hugo59200@gmail.com>
2022-02-09 23:04:32 +0100

    add pages/footer/navbar

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,28 @@ import nunjucks from "nunjucks";
 
 const app = express();
 
-app.use(express.static("public"));
-
 nunjucks.configure("views", {
   autoescape: true,
   express: app,
 });
 
+app.use(express.static("public"));
+
 app.set("view engine", "njk");
 
 app.get("/", (request, response) => {
-  response.send("home-page");
+  response.render("home-page");
+});
+
+app.get("/:page", (request, response) => {
+  const pages = ["about-me", "portfolio", "home-page"];
+  const routeParameters = request.params;
+  const findPage = pages.find((page) => page === routeParameters.page);
+  if(findPage){
+    response.render(request.params.page)
+  } else {
+    response.status(404).render("Error404", { error: "Page not found" });
+  }
 });
 
 app.listen(2500, () => {
